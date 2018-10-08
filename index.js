867b8bb6f9b2991b61fa344810d6b3e03c89bbce
Trevor Jameson <github email address>
2018-10-08 17:08:16 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Code your solution in this file!
+
+function logDriverNames(arr) {
+  arr.forEach( (elem) => {
+    console.log(elem.name);
+  })
+}
+
+function logDriversByHometown(arr, home) {
+  arr.forEach( (elem) => {
+    if (elem.hometown === home) {
+      console.log(elem.name);
+    }
+  })
+}
+
+function driversByRevenue(arr) {
+  let newArr = [...arr];
+  return newArr.sort( (a, b) => {
+    return a.revenue - b.revenue;
+  })
+}
+
+function driversByName(arr) {
+  let newArr = [...arr];
+  return newArr.sort( (a, b) => {
+    return a.name.localeCompare(b.name);
+  })
+}
+
+function totalRevenue(arr) {
+  let initialValue = 0;
+  return arr.reduce( (sum, obj) => {
+    return sum + obj.revenue;
+  }, initialValue)
+}
+
+function averageRevenue(arr) {
+  return (totalRevenue(arr) / arr.length)
+}
